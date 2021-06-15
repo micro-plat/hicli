b0d1370352fbb4a75e2249f9d23f0eb7111b6fec
hj <546921722@qq.com>
2021-06-15 11:17:59 +0800

    修改bug

--- markdown/ui/tmpl.detail.vue.go
+++ markdown/ui/tmpl.detail.vue.go
@@ -123,7 +123,7 @@ const TmplDetail = `
               </tbody>
             </table>
           </div>
-        {{- else if (index $tab.TabInfo.TabTableList $name) }}{{panelbody=true}}
+        {{- else if (index $tab.TabInfo.TabTableList $name) }}{{$panelbody = true}}
           <el-scrollbar style="height:100%" id="panel-body">
             <el-table :data="{{$tab.Name|rmhd|varName}}List.items" size="small" stripe style="width: 100%" :height="maxHeight">
               {{if gt $tab.ELTableIndex 0}}<el-table-column type="index" fixed	:index="indexMethod"></el-table-column>{{end}}
