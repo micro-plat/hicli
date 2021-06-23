9bc3d5acc90968f8ea6266b75f72c51a6b9ff648
hj <546921722@qq.com>
2021-06-23 19:57:00 +0800

    修改数据填充

--- markdown/ui/tmpl.add.vue.go
+++ markdown/ui/tmpl.add.vue.go
@@ -25,9 +25,9 @@ const TmplCreateVue = `
 			{{- else if $c.Con|SL }}
 			<el-form-item label="{{$c.Desc|shortName}}:" prop="{{$c.Name}}">
 				<el-select size="small" style="width: 100%;"	v-model="addData.{{$c.Name}}"	clearable filterable class="input-cos" placeholder="---请选择---"
-				{{- if or (cDicPName $c.Con $tb) (cGroupPName $c.Con $tb) }} @change="handleChooseTool()"{{$choose = true}}{{end}}
-				{{- if (cDicCName $c.Name $tb) }} @change="set{{(cDicCName $c.Name $tb)|upperName}}(addData.{{$c.Name}})" {{- end}}
-				{{- if (cGroupCName $c.Name $tb) }} @change="set{{$c.Name|upperName}}Group" {{- end}}	>
+				{{- if or (cDicPName $c.Con $tb) (cGroupPName $c.Con $tb) }} @change="handleChooseTool()"{{$choose = true}}
+				{{- else if (cDicCName $c.Name $tb) }} @change="set{{(cDicCName $c.Name $tb)|upperName}}(addData.{{$c.Name}})"
+				{{- else if (cGroupCName $c.Name $tb) }} @change="set{{$c.Name|upperName}}Group" {{- end}}	>
 					<el-option v-for="(item, index) in {{$c.Name|lowerName}}" :key="index" :value="item.value" :label="item.name"></el-option>
 				</el-select>
 			</el-form-item>
@@ -130,6 +130,7 @@ export default {
 			if (obj){
 				{{- range $i,$c1:=(cgroup $c.Name $tb)}}
 				this.addData.{{$c1.Name}} = obj.{{$c1.Name}}
+				{{if (cDicPName $c.Con $tb)  }}this.set{{$c.Name|upperName}}(obj.{{$c1.Name}}){{end}}
 				{{- end}}
 			}
 		},
