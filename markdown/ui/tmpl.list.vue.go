8d53b39409509fbcb0a9fe894666e17b537f230b
hj <546921722@qq.com>
2021-06-25 17:05:51 +0800

    修改bug

--- markdown/ui/tmpl.list.vue.go
+++ markdown/ui/tmpl.list.vue.go
@@ -27,7 +27,7 @@ const TmplList = `
 					{{- if (qDicCName $c.Name $tb) }} @change="set{{(qDicCName $c.Name $tb)|upperName}}(queryData.{{$c.Name}})"
 					{{- else if (qGroupCName $c.Name $tb) }} @change="set{{$c.Name|upperName}}Group" 
 					{{- else if or (qGroupPName $c.Con $tb) (qDicPName $c.Con $tb) }} @change="handleChooseTool()"{{$choose = true}}{{- end}}	
-					{{- if (qGroupPName $c.Con $tb)}} disabled{{-end}} >
+					{{- if (qGroupPName $c.Con $tb)}} disabled{{- end}} >
 						<el-option value="" label="全部"></el-option>
 						<el-option v-for="(item, index) in {{$c.Name|lowerName}}" :key="index" :value="item.value" :label="item.name"></el-option>
 					</el-select>
